947c6a0ddedc3606b7aea3275d2893bfd0726093
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-11-07 14:01:40 +0200

    Add Semaphore class with wait and signal functionality
    Implemented Semaphore class with wait and signal methods.

--- src/Semaphore/Semaphore.java
+++ src/Semaphore/Semaphore.java
@@ -1,4 +1,23 @@
-package Semaphore;
 public class Semaphore {
-    
+    private int value;
+
+    public Semaphore(int value) {
+        this.value = value;
+    }
+
+    public synchronized void waitSem() {
+        while (value <= 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        value--;
+    }
+
+    public synchronized void signal() {
+        value++;
+        notify();
+    }
 }
